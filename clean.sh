37015f0b61c348ae501dfba862e47968064408e9
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-02-27 10:21:43 -0500

    clean.sh

---
+++ clean.sh
@@ -0,0 +1,11 @@
+if [ "$1" = "l" ]; then
+    MAKE_FILE="Makefile.local"
+    echo "Using Conda's OpenCV library and Makefile.local."
+elif [ "$1" = "p" ]; then
+    MAKE_FILE="Makefile.palmetto"
+    echo "Using Spack's OpenCV library and Makefile.palmetto."
+else
+    echo "Invalid argument. Please use 'l' for Conda or 'p' for Spack."
+    exit 1
+fi
+make -f $MAKE_FILE clean
