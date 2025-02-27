b6b43b7e8a5200f2cc6a14129b2fcbff90c60c54
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-02-27 10:10:17 -0500

    support both

--- compile.sh
+++ compile.sh
@@ -1,4 +1,22 @@
-make clean
-make
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-./blur test_image.png
+#!/bin/bash
+
+# Check the first argument and set LD_LIBRARY_PATH and Makefile accordingly
+if [ "$1" = "l" ]; then
+    export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+    MAKE_FILE="Makefile.local"
+    echo "Using Conda's OpenCV library and Makefile.local."
+elif [ "$1" = "p" ]; then
+    export LD_LIBRARY_PATH=$(spack location -i opencv)/lib64:$LD_LIBRARY_PATH
+    MAKE_FILE="Makefile.palmetto"
+    echo "Using Spack's OpenCV library and Makefile.palmetto."
+else
+    echo "Invalid argument. Please use 'l' for Conda or 'p' for Spack."
+    exit 1
+fi
+
+# Clean and build the project using the selected Makefile
+make -f $MAKE_FILE clean
+make -f $MAKE_FILE
+
+# Run the blur program with the test image
+./blur test_image.bmp
